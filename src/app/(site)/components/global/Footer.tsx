31dc3391009287390432135835743ca91a6160e5
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-11-29 01:47:01 +0800

    feat: footer done

--- src/app/(site)/components/global/Footer.tsx
+++ src/app/(site)/components/global/Footer.tsx
@@ -1,20 +1,82 @@
-export default function Footer() {
-  return (
-    <footer className="border-t border-zinc-800 mt-44">
-      <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-16 text-zinc-400">
-        <small className=" duration-200 font-mono">
-          All rights reserved &copy; {new Date().getFullYear()}
-        </small>
+//import icons
+import { Icon } from "@iconify/react";
 
-        <small className="hover:text-white duration-200">
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div className="mx-auto px-8 py-10 flex justify-between text-zinc-400">
+        {/* Left Section */}
+        <div className="justify-left pl-10">
+          {" "}
+          <p>Designed in Figma. Developed using MERN. Hosted on Heroku.</p>
+          <p>
+            {"\n"}© {currentYear}. All rights reserved. Yudhishthra Sugumaran.
+          </p>
+        </div>
+
+        {/* Right Section */}
+        <div
+          className="flex items-center space-x-10 pr-18"
+          // include a right border
+        >
           <a
-            href="https://github.com/Evavic44/sanity-nextjs-site"
+            href="https://linkedin.com/in/yudhishthra"
             target="_blank"
-            rel="noreferrer noopener"
+            className="hover:text-white transition duration-200 flex items-center border-r-2 border-zinc-300 pr-10"
           >
-            Prepared by <span className="text-green-400">Victor Eke</span>
+            {/* the icon and word should appear i the same line */}
+            <p>LINKEDIN</p>
+            <Icon
+              icon="iconamoon:arrow-top-right-1-thin"
+              width="24"
+              height="24"
+              style={{ color: "#2f5597" }}
+              className="inline mb-5"
+            />
           </a>
-        </small>
+          <a
+            href="https://ctftime.org/team/177670"
+            target="_blank"
+            className="hover:text-white transition duration-200 flex items-center border-r-2 border-zinc-300 pr-10"
+          >
+            <p>CTFTIME</p>
+            <Icon
+              icon="iconamoon:arrow-top-right-1-thin"
+              width="24"
+              height="24"
+              style={{ color: "#2f5597" }}
+              className="inline mb-5"
+            />
+          </a>
+          <a
+            href="https://github.com/0xYudhishthra"
+            // open in new tab
+            target="_blank"
+            className="hover:text-white transition duration-200 flex items-center"
+          >
+            <p>GITHUB</p>
+            <Icon
+              icon="iconamoon:arrow-top-right-1-thin"
+              width="24"
+              height="24"
+              style={{ color: "#2f5597" }}
+              className="inline mb-5"
+            />
+          </a>
+          <a
+            href="#"
+            className="hover:text-white transition duration-200 flex items-center pl-5"
+          >
+            <Icon
+              icon="material-symbols-light:arrow-circle-up-outline"
+              width="40"
+              height="40"
+              style={{ color: "#FF8C00" }}
+            />
+          </a>
+        </div>
       </div>
     </footer>
   );
