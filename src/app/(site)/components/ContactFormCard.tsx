cf1f204db14438e020823e767f42a35c661ced49
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2023-12-12 02:53:47 +0800

    feat: complete contact me section

--- src/app/(site)/components/ContactFormCard.tsx
+++ src/app/(site)/components/ContactFormCard.tsx
@@ -12,23 +12,22 @@ const contactFormContainerStyle: React.CSSProperties = {
 };
 
 const contactFormTitleStyle: React.CSSProperties = {
-  fontSize: "1.8rem",
+  fontSize: "1.5rem",
   fontWeight: "bold",
   color: "#003366",
   textAlign: "left", // Align text to the left
-  paddingTop: "0.5rem", // Adjust this as needed to control space below the title
+  marginBottom: "0.5rem", // Adjust this as needed to control space below the title
 };
 
 const contactFormSubtitleStyle: React.CSSProperties = {
-  fontSize: "1.45rem",
+  fontSize: "1.4rem",
   color: "#333",
   textAlign: "left", // Align text to the left
-  marginTop: "1.5rem", // Add margin to the top of the subtitle
+  marginBottom: "1.6rem", // Adjust this as needed to control space below the title
 };
 
 const formStyle: React.CSSProperties = {
   width: "100%",
-  paddingTop: "3rem",
 };
 
 const formGroupStyle: React.CSSProperties = {
@@ -75,108 +74,136 @@ const buttonContainerStyle: React.CSSProperties = {
 };
 
 const ContactFormCard: React.FC = () => {
-  const [loading, setLoading] = useState(false);
-  const [messageInfo, setMessageInfo] = useState("");
+  const [formStage, setFormStage] = useState(0); // 0 - default, 1 - sending, 2 - awaiting response, 3 - sent
+  const targetAddress = "0x07c513df12a4cf8470be5a032c380db6c1c4ae74";
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setLoading(true);
-    setMessageInfo("Message Sending...");
 
-    // Simulate sending message
+    const formElements = event.currentTarget
+      .elements as typeof event.currentTarget.elements & {
+      name: HTMLInputElement;
+      contactInfo: HTMLInputElement;
+      message: HTMLTextAreaElement;
+    };
+
+    const name = formElements.name.value;
+    const contactInfo = formElements.contactInfo.value;
+    const message = formElements.message.value;
+
+    // Make sure the form is not empty
+    if (name === "" || contactInfo === "" || message === "") {
+      alert("Please fill in all the fields");
+      return;
+    }
+
+    setFormStage(1); // Start sending process
+
+    // Simulate the sending process
     await new Promise((resolve) => setTimeout(resolve, 2000));
 
-    setLoading(false);
-    setMessageInfo(
-      "Message sent to 0x8dC75164dab325Dda81022cE7ee7a2B23a207C04, powered by Blockscan Chat"
-    );
+    // Call the backend API to send the message
+    const res = await fetch("/api/contactForm", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        targetAddress: targetAddress,
+        name: name,
+        contactInfo: contactInfo,
+        message: message,
+      }),
+    });
+
+    setFormStage(2); // Awaiting response
+
+    // Simulate awaiting response
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    setFormStage(3); // Message sent
+
+    // Reset form after a delay
+    setTimeout(() => setFormStage(0), 5000);
+  };
+
+  const getButtonText = () => {
+    switch (formStage) {
+      case 1:
+        return `Sending message to ${targetAddress}...`;
+      case 2:
+        return "Awaiting response";
+      case 3:
+        return `Message sent to ${targetAddress}, I'll reply to you as soon as I can!`;
+      default:
+        return "Send message";
+    }
   };
 
   return (
-    <>
-      <div style={contactFormContainerStyle}>
-        <h2 style={contactFormTitleStyle}>Let's talk!</h2>
-        <p style={contactFormSubtitleStyle}>
-          I’m interested in freelance opportunities - especially projects that
-          create real-world impact. However, if you’d like to just say hello,
-          don’t hesitate to drop your message!
-        </p>{" "}
-        <form style={formStyle} onSubmit={handleSubmit}>
-          <div style={formGroupStyle}>
-            <label style={labelStyle} htmlFor="name">
-              What can I call you?
-            </label>
-            <input
-              style={inputStyle}
-              type="text"
-              id="name"
-              name="name"
-              placeholder=""
-            />
-          </div>
-          <div style={formGroupStyle}>
-            <label style={labelStyle} htmlFor="email">
-              How I can reach out to you?
-            </label>
-            <input
-              style={inputStyle}
-              type="email"
-              id="email"
-              name="email"
-              placeholder=""
-            />
-          </div>
-          <div style={formGroupStyle}>
-            <label style={labelStyle} htmlFor="message">
-              What are you reaching out for?
-            </label>
-            <textarea
-              style={inputStyle}
-              id="message"
-              name="message"
-              placeholder=""
-              rows={4}
-            ></textarea>
-          </div>
-          <div style={buttonContainerStyle}>
-            <button style={buttonStyle} type="submit" disabled={loading}>
-              {loading ? "Sending..." : "Send message"}
-            </button>
-            {loading && (
-              <div
-                style={{
-                  width: "100%",
-                  backgroundColor: "#ddd",
-                  height: "5px",
-                  marginTop: "10px",
-                }}
-              >
-                <div
-                  style={{
-                    width: "100%",
-                    height: "5px",
-                    backgroundColor: "#e76e54",
-                    animation: "loading 2s infinite",
-                  }}
-                />
-              </div>
-            )}
-          </div>
-        </form>
-        {messageInfo && (
-          <div
-            style={{
-              position: "absolute",
-              right: "10px",
-              bottom: "10px",
-              fontSize: "0.8rem",
-            }}
-          >
-            {messageInfo}
-          </div>
-        )}
+    <div style={contactFormContainerStyle}>
+      <h2 style={contactFormTitleStyle}>Let's talk!</h2>
+      <p style={contactFormSubtitleStyle}>
+        I’m interested in freelance opportunities - especially projects that
+        create real-world impact. However, if you’d like to just say hello,
+        don’t hesitate to drop your message!
+      </p>
+
+      <form style={formStyle} onSubmit={handleSubmit}>
+        <div style={formGroupStyle}>
+          <label style={labelStyle} htmlFor="name">
+            What can I call you?
+          </label>
+          <input
+            style={inputStyle}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Anon"
+          />
+        </div>
+        <div style={formGroupStyle}>
+          <label style={labelStyle} htmlFor="contactInfo">
+            How I can reach out to you?
+          </label>
+          <input
+            style={inputStyle}
+            type="email"
+            id="contactInfo"
+            name="contactInfo"
+            placeholder="anon@hashmail.dev"
+          />
+        </div>
+        <div style={formGroupStyle}>
+          <label style={labelStyle} htmlFor="message">
+            What are you reaching out for?
+          </label>
+          <textarea
+            style={inputStyle}
+            id="message"
+            name="message"
+            placeholder="Work on the next big thing"
+            rows={4}
+          ></textarea>
+        </div>
+        <div style={buttonContainerStyle}>
+          <button style={buttonStyle} type="submit" disabled={formStage !== 0}>
+            {getButtonText()}
+          </button>
+        </div>
+      </form>
+
+      <div
+        style={{
+          position: "absolute",
+          right: "15px",
+          bottom: "15px",
+          fontSize: "1rem",
+        }}
+      >
+        This feature is powered by{" "}
+        <a href="https://chat.blockscan.com/" target="_blank">
+          <u>Blockscan Chat.</u>
+        </a>
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ src/app/api/contactForm/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  //format a custom message format that says:-
+  const constructMessage = (
+    name: string,
+    contactInfo: string,
+    message: string
+  ) => {
+    return `New Message Received:- 
+      \n\n
+      Name: ${name}\n\n
+      \n\nContact Info: ${contactInfo}\n\n
+      \n\nMessage: ${message}`;
+  };
+
+  const requestBody = await request.json();
+  const res = await fetch(
+    `https://chatapi.blockscan.com/v1/api?&method=sendchat&to=${
+      requestBody.targetAddress
+    }&msg=${constructMessage(
+      requestBody.name,
+      requestBody.contactInfo,
+      requestBody.message
+    )}&apikey=${process.env.BLOCKSCAN_API_KEY}`
+  );
+
+  if (res.status === 200) {
+    const { status, message, result } = await res.json();
+    if (status === "1" && message === "OK") {
+      return NextResponse.json({ message: result }, { status: 200 });
+    }
+  } else {
+    return NextResponse.json({ message: "Error" }, { status: 500 });
+  }
+}
