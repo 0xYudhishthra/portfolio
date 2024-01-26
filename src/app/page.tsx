00f8e0f2cb529270ab54210f038d926f0f69063f
Yudhish Maran <66850474+0xYudhishthra@users.noreply.github.com>
2024-01-27 02:16:40 +0800

    Revert "feat: enable swipe"
    This reverts commit 2e320d592f9fa7288469810e2cf76d517a89c617.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-//page.tsx
-
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
-import { motion, useAnimation, AnimatePresence } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { Icon } from "@iconify/react";
 import {
   HeroCard,
@@ -16,37 +14,38 @@ import {
 /**
  * OVERALL STYLES
  */
-const arrowStyle: React.CSSProperties = {
+
+const arrowContainerStyle: React.CSSProperties = {
   position: "fixed",
-  right: "50px",
-  fontSize: "24px",
-  cursor: "pointer",
+  top: "50%",
+  right: "2%", // Anchor to the right side of the screen
+  transform: "translateY(-50%)", // Only translate vertically
+  display: "flex",
+  alignItems: "center",
   zIndex: 1000,
-  // Ensure transform origin is consistent
-  transformOrigin: "center",
+  flexDirection: "column",
 };
 
 const moveCardsTextStyle: React.CSSProperties = {
-  position: "fixed",
   right: "20px",
   top: "50%",
-  transform: "translateY(-50%) rotate(90deg)",
   color: "#FF8C00",
   fontSize: "18px",
   zIndex: 1000,
+  paddingTop: "20px",
+  paddingBottom: "20px",
 };
 
 const wrapperStyle: React.CSSProperties = {
   position: "relative",
+  width: "95%", // Adjust to the width of your sections
   height: "80vh", // Adjust to the height of your container
   overflow: "hidden", // Prevent scrolling to see the dragged section
   display: "flex",
-  flexDirection: "column-reverse", // Stack sections bottom to top
+  flexDirection: "column", // Stack sections bottom to top
   alignItems: "center",
   justifyContent: "center",
   zIndex: 1,
-  marginRight: "auto",
-  padding: "5%", // Use padding instead of width to control the size
 };
 
 const sectionStyle: React.CSSProperties = {
@@ -60,7 +59,7 @@ const sectionStyle: React.CSSProperties = {
   justifyContent: "center",
   alignItems: "center",
   flexDirection: "column",
-  padding: "5%", // Use responsive padding
+  padding: "50px",
   boxSizing: "border-box",
   perspective: "1000px", // Add perspective for 3D effect
   border: "5px solid #FF8C00",
@@ -73,6 +72,22 @@ const Home = () => {
   const controls = useAnimation();
   const [loading, setLoading] = useState(false);
   const [messageInfo, setMessageInfo] = useState("");
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Function to check the window width
+    const checkDeviceSize = () => {
+      const match = window.matchMedia("(max-width: 768px)").matches;
+      setIsMobile(match);
+    };
+
+    // Check once and also add event listener for resizing
+    checkDeviceSize();
+    window.addEventListener("resize", checkDeviceSize);
+
+    // Clean up event listener
+    return () => window.removeEventListener("resize", checkDeviceSize);
+  }, []);
 
   type ArrowProps = {
     direction: "up" | "down";
@@ -80,27 +95,18 @@ const Home = () => {
   };
 
   const Arrow: React.FC<ArrowProps> = ({ direction, onClick }) => (
-    <div
+    <Icon
       onClick={onClick}
-      style={{
-        ...arrowStyle,
-        top: "50%",
-        transform:
-          direction === "up" ? "translateY(-250%)" : "translateY(150%)",
-      }}
       className="blink"
-    >
-      <Icon
-        icon={
-          direction === "up"
-            ? "material-symbols-light:arrow-circle-up-outline"
-            : "material-symbols-light:arrow-circle-down-outline"
-        }
-        width="40"
-        height="40"
-        style={{ color: "#FF8C00" }}
-      />
-    </div>
+      icon={
+        direction === "up"
+          ? "material-symbols-light:arrow-circle-up-outline"
+          : "material-symbols-light:arrow-circle-down-outline"
+      }
+      width="40"
+      height="40"
+      style={{ color: "#FF8C00", cursor: "pointer", zIndex: 1000 }}
+    />
   );
 
   const sectionsData = [
@@ -150,6 +156,18 @@ const Home = () => {
     });
   };
 
+  // Function to handle end of drag event
+  const handleDragEnd = (event: any, info: any) => {
+    const offset = info.offset.y;
+    const velocity = info.velocity.y;
+
+    if (offset < 0 || velocity < -10) {
+      updateSections("up");
+    } else if (offset > 0 || velocity > 10) {
+      updateSections("down");
+    }
+  };
+
   useEffect(() => {
     controls.start((i) => ({
       y: i * -SECTION_OFFSET,
@@ -166,11 +184,16 @@ const Home = () => {
 
   return (
     <div style={wrapperStyle}>
-      <Arrow direction="up" onClick={() => updateSections("up")} />
-      <div style={moveCardsTextStyle}>move cards</div>
+      {/* Arrow container */}
+      {!isMobile && (
+        <div style={arrowContainerStyle}>
+          <Arrow direction="up" onClick={() => updateSections("up")} />
+          <div style={moveCardsTextStyle}>move cards</div>
+          <Arrow direction="down" onClick={() => updateSections("down")} />
+        </div>
+      )}
       {sections.map((section, index) => (
         <motion.div
-          className="absolute w-full max-w-screen-lg mx-auto rounded-lg p-4 md:p-8"
           custom={index}
           animate={controls}
           key={section.id}
@@ -179,12 +202,14 @@ const Home = () => {
             backgroundColor: section.color,
             zIndex: sections.length - index, // Ensures proper stacking
           }}
+          drag="y" // Enable dragging on y-axis
+          dragConstraints={{ top: 0, bottom: 0 }} // Constrain drag to vertical axis
+          dragElastic={1} // Elasticity of drag
+          onDragEnd={handleDragEnd} // Handle the drag end event
         >
           {section.content}
         </motion.div>
       ))}
-
-      <Arrow direction="down" onClick={() => updateSections("down")} />
     </div>
   );
 };
